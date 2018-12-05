a5d9150d3db349b991ef9478b09d4f12d5003e38
Natalie9 <natalietolentino9@gmail.com>
2018-12-05 12:35:31 -0200

    Validação de partida vencida, gravação no banco, td clicáveis e nova partida

--- public/index.js
+++ public/index.js
@@ -1,44 +1,81 @@
 var database = firebase.database();
 
-var cont=1;
-var carac='O';
+var jogador = 1;
+var caracter = 'O';
+
+var jogadas = "";
+var posicoes = new Array();
+
+var jogo = 1;
+
+var cont=0; 
+
 function jogar(event, posicao) {
-    var posicao = event.posicao;
-    console.log(posicao);
-    firebase.database().ref('posicao/' +carac).set(posicao);
-    validarUsuario();
-    var i;
-    for (i = 1; i <= 9; i++) {
-        if (posicao == i) {
-            document.getElementById(i).innerHTML = carac;
-        }
-    }   
-}
-function teste(event, posicao){
-    console.log(posicao);
-}
+    cont++;
+    validarUsuario();   
+    posicoes[posicao] = caracter;
 
-function text(event, posicao) {
-    var texto = event.value;
-    firebase.database().ref('texto/' + cont + '/' +campo).set(texto);
-}
-function validarUsuario(){
+    jogadas = posicoes.toString();
+   
+    firebase.database().ref('Jogo '+ jogo +'/Jogadas/').set(jogadas);
     
-    if (cont==1){
-        carac ="O";
-    }
-    else{
-        carac ="X";
-    }
+    document.getElementById(posicao).innerHTML = caracter;
+
+    testarVitoria();
 }
 
-function decrescer(){
-    cont = 1;
-    document.getElementById('usuario').innerHTML='1';
-    validarUsuario();
+function validarUsuario() {
+    if (jogador == 1) {
+        acrescentar();
+        caracter = "O";
+    }
+    else {
+        caracter = "X";
+        decrescer();
+    }
 }
-function acrescentar(){
-        cont = 2;
-    document.getElementById('usuario').innerHTML='2';
-    validarUsuario();
+function vencido(mensagem){
+    alert("Fim de jogo! "+ mensagem);
+    jogo++;
+    cont =0;
+   
+    var i;
+    for(i=0;i<=8;i++){
+        document.getElementById(i).innerHTML = '';
+        posicoes[i]=null;
+    }
+}
+function decrescer() {
+    jogador = 1;
+    document.getElementById('usuario').innerHTML = '1';
+}
+function acrescentar() {
+    jogador = 2;
+    document.getElementById('usuario').innerHTML = '2';
+}
+
+function testarVitoria() {
+    var linha, coluna;
+    for(linha=0; linha<=8; linha=linha+3){
+        if(posicoes[linha]==posicoes[linha+1]&&posicoes[linha]==posicoes[linha+2]&&posicoes[linha+1]!=null){
+            setTimeout(function(){ vencido("Jogador " + caracter + " venceu!");}, 100);
+        }
+        
+    }
+    for(coluna=0; coluna<=8; coluna++){
+        if(posicoes[coluna]==posicoes[coluna+3]&&posicoes[coluna]==posicoes[coluna+6]&&posicoes[coluna]!=null){
+            setTimeout(function(){ vencido("Jogador " + caracter + " venceu!");}, 100);
+        }
+    }
+    if(posicoes[0]==posicoes[4]&&posicoes[0]==posicoes[8]&&posicoes[0]!=null){
+        setTimeout(function(){ vencido("Jogador " + caracter + " venceu!");}, 100);
+    }
+    if(posicoes[2]==posicoes[4]&&posicoes[2]==posicoes[6]&&posicoes[2]!=null){
+        setTimeout(function(){ vencido("Jogador " + caracter + " venceu!");}, 100);
+    }
+    if(cont==9){
+        setTimeout(function(){ vencido("Empate!"); }, 100);
+        
+    }
+
 }
